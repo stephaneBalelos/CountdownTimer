3a99f7ea50d34a9a6a7f7214efca43f47499591a
stephaneBalelos <stephanedondyas2@gmail.com>
2024-10-10 10:51:27 +0200

    Expiration Message

--- src/countdown-timer.ts
+++ src/countdown-timer.ts
@@ -18,27 +18,44 @@ export function setupTimers() {
       continue
     }
 
+    const expiredMessageEl = element.querySelector('[data-expired-message]')
+    const expiredMessage = expiredMessageEl?.innerHTML ?? 'Expired'
+    expiredMessageEl?.remove()
+
     const daysUnits = [daysElement.querySelector('[data-unit]')?.innerHTML ?? 'day', daysElement.querySelector('[data-days]')?.innerHTML ?? 'days']
     const hoursUnits = [hoursElement.querySelector('[data-init]')?.innerHTML ?? 'hour', hoursElement.querySelector('[data-inits]')?.innerHTML ?? 'hours']
     const minutesUnits = [minutesElement.querySelector('[data-unit]')?.innerHTML ?? 'minute', minutesElement.querySelector('[data-units]')?.innerHTML ?? 'minutes']
     const secondsUnits = [secondsElement.querySelector('[data-unit]')?.innerHTML ?? 'seconds', secondsElement.querySelector('[data-units]')?.innerHTML ?? 'seconds']
 
 
-    const { days, hours, minutes, seconds } = update(date)
+    const initialValues = update(date)
+    if (!initialValues) {
+      element.innerHTML = expiredMessage
+      continue
+    }
+    const { days, hours, minutes, seconds } = initialValues
     render(daysElement, days, daysUnits)
     render(hoursElement, hours, hoursUnits)
     render(minutesElement, minutes, minutesUnits)
     render(secondsElement, seconds, secondsUnits)
 
-    setInterval(() => {
+    const interval = setInterval(() => {
       
       const values = update(date)
+
+      if (!values) {
+        clearInterval(interval)
+        element.innerHTML = expiredMessage
+        return
+      }
 
       render(daysElement, values.days, daysUnits)
       render(hoursElement, values.hours, hoursUnits)
       render(minutesElement, values.minutes, minutesUnits)
       render(secondsElement, values.seconds, secondsUnits)
+
     }, 1000)
+
   }
 
 }
@@ -52,6 +69,10 @@ const render = (el: Element, value: number, units: (string | undefined)[]) => {
 const update = (date: Date) => {
   const now = new Date()
   const timeLeft = date.getTime() - now.getTime()
+
+  if (timeLeft < 0) {
+    return null
+  }
 
   const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
